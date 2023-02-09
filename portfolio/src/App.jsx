59120e976dc9381aa8584c1dd0859a7f38acc361
joan-vandenbulcke <vandenbulckejoan@gmail.com>
2023-02-09 13:34:58 +0100

    Initial Commit

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import AppHeader from './Components/AppHeader'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <AppHeader />
+  )
+}
+
+export default App
