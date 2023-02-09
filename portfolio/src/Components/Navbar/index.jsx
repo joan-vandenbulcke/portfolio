de5035eac37055a3b3b88e95534c203ca9e94896
joan-vandenbulcke <vandenbulckejoan@gmail.com>
2023-02-09 15:36:52 +0100

    Add Navbar and CSS

--- portfolio/src/Components/Navbar/index.jsx
+++ portfolio/src/Components/Navbar/index.jsx
@@ -1,13 +1,35 @@
+import './styles.css';
+import reactLogo from '../../assets/react.svg';
+
 const Navbar = () => (
   <div className="navbar">
     <nav className="navbar-container">
-      <ul className="navbar-container-list">
-        <li className="navbar-container-list__item">Test 1</li>
-        <li className="navbar-container-list__item">Test 1</li>
-        <li className="navbar-container-list__item">Test 1</li>
-        <li className="navbar-container-list__item">Test 1</li>
-        <li className="navbar-container-list__item">Test 1</li>
-      </ul>
+      <img src={reactLogo} className="logo" alt="logo portfolio" />
+      <div className="navbar-right">
+        <ol className="navbar-links">
+          <li
+            className="navbar-item">
+            <a className="navbar-item-link" href="">About</a>
+          </li>
+          <li
+            className="navbar-item">
+            <a className="navbar-item-link" href="">Experience</a>
+          </li>
+          <li
+            className="navbar-item">
+            <a className="navbar-item-link" href="">Work</a>
+          </li>
+          <li
+            className="navbar-item">
+            <a className="navbar-item-link" href="">School</a>
+          </li>
+          <li
+            className="navbar-item">
+            <a className="navbar-item-link" href="">Contact</a>
+          </li>
+        </ol>
+        <button className="menu-button">Resume</button>
+      </div>
     </nav>
   </div>
 );
