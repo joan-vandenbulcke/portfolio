d5edd8ba8cc3444c73700ef9f12859b12e9333e5
joan-vandenbulcke <vandenbulckejoan@gmail.com>
2023-02-12 12:01:55 +0100

    Add Work and bit of css / STATIC

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -4,6 +4,7 @@ import './App.css';
 import AppHeader from './Components/AppHeader';
 import Content from './Components/Content';
 import About from './Components/About';
+import Work from './Components/Work';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -13,6 +14,7 @@ function App() {
       <AppHeader />
       <Content />
       <About />
+      <Work />
     </div>
   )
 }
