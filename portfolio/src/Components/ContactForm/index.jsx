cde39c4202438489897d9c1c488adaa7ca65318a
joan-vandenbulcke <vandenbulckejoan@gmail.com>
2023-03-15 22:10:27 +0100

    some modif

--- portfolio/src/Components/ContactForm/index.jsx
+++ portfolio/src/Components/ContactForm/index.jsx
@@ -1,7 +1,7 @@
 import './styles.scss';
 
 import emailjs from '@emailjs/browser';
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import { BsSendFill } from "react-icons/bs";
 
 const ContactForm = () => {
@@ -18,24 +18,36 @@ const ContactForm = () => {
         console.log(error.text);
       });
   }
+  // ------------- SUCCESS MESSAGE --------------------
+  const [isSuccessMessage, setIsSuccessMessage] = useState([]);
+
+  const Message = () => (
+    <p>Votre message a bien été envoyé</p>
+  )
+
+  const successMessage = (e) => {
+    console.log('Votre message a bien été envoyé');
+    setIsSuccessMessage(isSuccessMessage.concat(<Message key={isSuccessMessage.length} />));
+  }
 
   return (
     <section className='form'>
       <h3 className='form-title'><span className='numbers-span'>05.</span>Contact</h3>
       <form ref={form} onSubmit={sendEmail}>
         <div className='form-item'>
-          <label>Name</label>
-          <input type="text" name="from_name" />
+          <label><span className='form-span'>*</span> Name</label>
+          <input required type="text" name="from_name" />
         </div>
         <div className='form-item'>
-          <label>Email</label>
-          <input type="email" name="from_email" />
+          <label><span className='form-span'>*</span> Email</label>
+          <input required type="email" name="from_email" />
         </div>
         <div className='form-item'>
-          <label>Message</label>
-          <textarea name="message" />
+          <label><span className='form-span'>*</span> Message</label>
+          <textarea required name="message" />
         </div>
-        <button className='form-button' type="submit">Send <BsSendFill /></button>
+        <button onClick={successMessage} className='form-button' type="submit">Send <BsSendFill /></button>
+        {Message}
       </form>
     </section>
   )
