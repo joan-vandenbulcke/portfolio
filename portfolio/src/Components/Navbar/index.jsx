bc7aefc209584ef2647833812ce6e1affc8e1632
joan-vandenbulcke <vandenbulckejoan@gmail.com>
2023-03-18 18:12:20 +0100

    add burger menu (in progress)

--- portfolio/src/Components/Navbar/index.jsx
+++ portfolio/src/Components/Navbar/index.jsx
@@ -28,14 +28,16 @@ const Navbar = () => {
   // --------------------------
   return (
     <div id='navbar' className="navbar">
-      <nav className="navbar-container">
+      <nav className={`navbar-container ${showLinks ? "show-navbar" : "hide-nav"}`}>
         <Link onClick={scrollToTop} className='navbar-link__logo'>
           <p className='navbar-logo'>JV</p>
         </Link>
         <div className="navbar-right">
           <ol className="navbar-links">
             <li
-              className="navbar-item">
+              className="navbar-item"
+              onClick={handleShowLinks}
+            >
               <ScrollLink
                 to='about'
                 spy={true}
@@ -47,7 +49,9 @@ const Navbar = () => {
               </ScrollLink>
             </li>
             <li
-              className="navbar-item">
+              className="navbar-item"
+              onClick={handleShowLinks}
+            >
               <ScrollLink
                 to='work'
                 spy={true}
@@ -60,41 +64,50 @@ const Navbar = () => {
               {/* <a className="navbar-item-link" href="#work"><span className='navbar-span'>2. </span>Expériences</a> */}
             </li>
             <li
-              className="navbar-item">
+              className="navbar-item"
+              onClick={handleShowLinks}
+            >
               <ScrollLink
                 to='projects'
                 spy={true}
                 smooth={true}
                 duration={500}
-                offset={-150}
                 className="navbar-item-link">
                 <span className='navbar-span'>3.  </span>Projects
               </ScrollLink>
             </li>
             <li
-              className="navbar-item">
+              className="navbar-item"
+              onClick={handleShowLinks}
+            >
               <ScrollLink
                 to='skills'
                 spy={true}
                 smooth={true}
+                offset={-100}
                 duration={500}
                 className="navbar-item-link">
                 <span className='navbar-span'>4.  </span>Skills
               </ScrollLink>
             </li>
             <li
-              className="navbar-item">
+              className="navbar-item"
+              onClick={handleShowLinks}
+            >
               <ScrollLink
                 to='school'
                 spy={true}
                 smooth={true}
+                offset={-100}
                 duration={500}
                 className="navbar-item-link">
                 <span className='navbar-span'>5.  </span>School
               </ScrollLink>
             </li>
             <li
-              className="navbar-item">
+              className="navbar-item"
+              onClick={handleShowLinks}
+            >
               <ScrollLink
                 to='contact'
                 spy={true}
@@ -104,13 +117,13 @@ const Navbar = () => {
                 <span className='navbar-span'>6.  </span>Contact
               </ScrollLink>
             </li>
+            <a className='navbar-display__pdf' href="src\assets\cv-joan-vandenbulcke.pdf" target="_blank">
+              <button className="menu-button">CV / Resume <ArrowCircleDownRoundedIcon className='navbar-download__icon icon' /></button>
+            </a>
           </ol>
-          <button className='navbar-burger'>
+          <button onClick={handleShowLinks} className='navbar-burger'>
             <span className='burger-bar'></span>
           </button>
-          <a className='navbar-display__pdf' href="src\assets\cv-joan-vandenbulcke.pdf" target="_blank">
-            <button className="menu-button">CV / Resume <ArrowCircleDownRoundedIcon className='navbar-download__icon icon' /></button>
-          </a>
         </div>
       </nav>
     </div>
